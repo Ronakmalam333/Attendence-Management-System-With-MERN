fdc97c3f9bc4fd4f7ef59ed3585e63165fafc816
Ronak <itzronakmalam94@gmail.com>
2025-01-26 02:01:03 +0530

    Register Page Middle Push Incomplete For Vikas

--- attendence/src/App.jsx
+++ attendence/src/App.jsx
@@ -2,14 +2,13 @@
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
-
+import Register from './components/register/Register'
 function App() {
 
   return (
     <>
       <div className="container">
-        <Navbar />
-        <Home />
+        <Register/>
       </div>
     </>
   )
--- attendence/src/components/register/Register.jsx
+++ attendence/src/components/register/Register.jsx
@@ -1,13 +1,64 @@
-
-import React from 'react'
-import './register.css'
+import React, { useState } from "react";
+import "./register.css";
 
 function Register() {
+  const [uid, setUid] = useState("");
+  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let hasError = false;
+
+    if (!uid) {
+      setError((prevError) => ({ ...prevError, uid: "Please enter your UID" }));
+      hasError = true;
+    } else {
+      setError((prevError) => ({ ...prevError, uid: "" }));
+    }
+
+    if (!password) {
+      setError((prevError) => ({
+        ...prevError,
+        password: "Please enter your password",
+      }));
+      hasError = true;
+    } else {
+      setError((prevError) => ({ ...prevError, password: "" }));
+    }
+
+    if (!hasError) {
+      setFormData({ uid, password });
+      console.log("Form Data Submitted:", { uid, password });
+    }
+  };
+
   return (
-    <div>
-      register
+    <div className='register'>
+      <h1>Register User</h1>
+      <form id='form' onSubmit={handleSubmit}>
+        <label htmlFor='uid'>
+          UID:
+          <input type='text' id='uid' name='uid' value={uid} placeholder='Enter your UID...' onChange={(e) => setUid(e.target.value)} />
+          {error.uid && <p style={{ color: "red" }}>{error.uid}</p>}
+        </label>
+        <label htmlFor='password'>
+          Password:
+          <input type='password' id='password' name='password' value={password} placeholder='Enter your password...' onChange={(e) => setPassword(e.target.value)}/>
+          {error.password && <p style={{ color: "red" }}>{error.password}</p>}
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+      {formData.uid && (
+        <div className='success-message'>
+          <h3>Form Submitted Successfully</h3>
+          <p>UID: {formData.uid}</p>
+          <p>Password: {formData.password}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
