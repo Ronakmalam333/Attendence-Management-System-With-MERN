1db338380909ef18a938621ee34a245acbe2e169
jugendrakashyap <kjugendra63@gmail.com>
2025-02-19 23:49:24 +0530

    save all changes

---
+++ client/src/components/context/Schedule.jsx
@@ -0,0 +1,171 @@
+import React, { createContext } from 'react'
+
+export const scheduleContext = createContext()
+export function Schedule({ children }) {
+    const mon = [{
+        "sub": "FERAJ",
+        "start": "09:15 AM",
+        "end": "10:15 AM",
+        "faculty": "Adil Ahmed"
+    },
+    {
+        "sub": "BENJ",
+        "start": "10:15 AM",
+        "end": "11:15 AM",
+        "faculty": "Adil Ahmed"
+    },
+    {
+        "sub": "ND",
+        "start": "11:15 AM",
+        "end": "12:15 AM",
+        "faculty": "Sakshi Kasera"
+    },
+    {
+        "sub": "OOPIC",
+        "start": "01:00 PM",
+        "end": "03:00 PM",
+        "faculty": "Adil Ahmed"
+    },
+    {
+        "sub": "ND",
+        "start": "03:00 PM",
+        "end": "04:00 PM",
+        "faculty": "Sakshi Kasera"
+    }]
+
+    const tue = [{
+        "sub": "FERAJ",
+        "start": "09:15 AM",
+        "end": "10:15 AM",
+        "faculty": "Adil Ahmed"
+    },
+    {
+        "sub": "ES",
+        "start": "10:15 AM",
+        "end": "11:15 AM",
+        "faculty": "Dhruv Trivedi"
+    },
+    {
+        "sub": "OOPIC",
+        "start": "11:15 AM",
+        "end": "12:15 AM",
+        "faculty": "Ashutosh Pande"
+    },
+    {
+        "sub": "Library",
+        "start": "01:00 PM",
+        "end": "03:00 PM"
+    },
+    {
+        "sub": "Maths-II",
+        "start": "03:00 PM",
+        "end": "04:00 PM",
+        "faculty": "Vardhan Parmar"
+    },
+    {
+        "sub": "Sports",
+        "start": "03:00 PM",
+        "end": "04:00 PM"
+    }]
+
+    const wed = [{
+        "sub": "Library",
+        "start": "09:15 AM",
+        "end": "10:15 AM"
+    },
+    {
+        "sub": "ND",
+        "start": "10:15 AM",
+        "end": "12:15 AM",
+        "faculty": "Sakshi Kasera"
+    },
+    {
+        "sub": "Maths-II",
+        "start": "11:15 AM",
+        "end": "12:15 AM",
+        "faculty": "Vardhan Parmar"
+    },
+    {
+        "sub": "Sports",
+        "start": "01:00 PM",
+        "end": "03:00 PM"
+    },
+    {
+        "sub": "Free",
+        "start": "03:00 PM",
+        "end": "04:00 PM"
+    }]
+
+    const thu = [{
+        "sub": "OOPIC",
+        "start": "09:15 AM",
+        "end": "10:15 AM",
+        "faculty": "Ashutosh Pande"
+    },
+    {
+        "sub": "ES",
+        "start": "10:15 AM",
+        "end": "11:15 AM",
+        "faculty": "Dhruv Trivedi"
+    },
+    {
+        "sub": "BENJ",
+        "start": "11:15 AM",
+        "end": "12:15 AM",
+        "faculty": "Adil Ahmed"
+    },
+    {
+        "sub": "FERAJ",
+        "start": "01:00 PM",
+        "end": "02:00 PM",
+        "faculty": "Adil Ahmed"
+    },
+    {
+        "sub": "FERAJ",
+        "start": "02:00 PM",
+        "end": "04:00 PM",
+        "faculty": "Adil Ahmed"
+    }]
+    const fri = [{
+        "sub": "BENJ",
+        "start": "09:15 AM",
+        "end": "11:15 AM",
+        "faculty": "Adil Ahmed"
+    },
+    {
+        "sub": "OOPSIC",
+        "start": "11:15 AM",
+        "end": "12:15 AM",
+        "faculty": "Ashutosh Pande"
+    },
+    {
+        "sub": "Maths-II",
+        "start": "01:0 AM",
+        "end": "02:00 AM",
+        "faculty": "Vardhan Parmar"
+    },
+    {
+        "sub": "ND",
+        "start": "02:00 PM",
+        "end": "03:00 PM",
+        "faculty": "Sakshi Kasera"
+    },
+    {
+        "sub": "BENJ",
+        "start": "03:00 PM",
+        "end": "04:00 PM",
+        "faculty": "Adil Ahmed"
+    }]
+
+    const leave = [{
+        "sub": "Today is Leave",
+        "start": "",
+        "end": ""
+    }]
+
+    return (
+        <scheduleContext.Provider value={{ mon, tue, wed, thu, fri, leave }}>
+            {children}
+        </scheduleContext.Provider>
+    )
+}
