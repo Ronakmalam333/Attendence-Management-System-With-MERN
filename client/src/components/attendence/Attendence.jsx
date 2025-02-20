f7f455dfc801adf0f47fbfff57762477aedc17a0
Ronak <itzronakmalam94@gmail.com>
2025-02-21 01:01:57 +0530

    Created Attendence And Help Component With It's Css Files

---
+++ client/src/components/attendence/Attendence.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Attendence = () => {
+  return (
+    <div className='attemdence-container'>
+      <div className='attendence-title'>
+        <h1>Attendence</h1>
+      </div>
+
+      <div className='attendence-table'>
+        {/*Including Attendence Excel Sheet Like Table Here */}
+      </div>
+
+      <div className="attendence-chart">
+        {/*Including Pie Chart Here In This Section*/}
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default Attendence;
