7698dec7d7c91accd0d7b83a4b2d6e8fa174c5b7
anmo2834 <anmolsinha4321@gmail.com>
2025-02-20 14:20:50 +0530

    styled token.jsx

--- client/src/components/home/token/Token.jsx
+++ client/src/components/home/token/Token.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 import './token.css'
 function Token() {
   return (
-    <div className='token_box'>
-      <h1 className='sub_name'>Subject Name</h1>
-      <input className='token_input' type="number" placeholder='Enter Subject Token' />
-      <button className='submit_btn'>Submit</button>
+    <div className='token_contain'>
+      <form className='token_box'>
+        <h1 className='sub_name'>Subject Name</h1>
+        <input className='token_input' type="text" placeholder='Enter Subject Token' />
+        <button className='submit_btn'>Submit</button>
+      </form>
     </div>
   )
 }
