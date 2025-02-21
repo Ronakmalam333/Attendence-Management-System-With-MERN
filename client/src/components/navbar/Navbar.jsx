b1beebc4df23607a0485a41ce00b4f0877cc0dd8
anmo2834 <anmolsinha4321@gmail.com>
2025-02-21 22:55:22 +0530

    given margin to the schedule container and aligned menu bar with media query

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -22,6 +22,9 @@ function Navbar() {
 
   return (
     <div className='nav-contain'>
+      <div className="hamburger">
+      <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#000000"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+      </div>
       <div className="logo"><h1>logo</h1></div>
       <div className="menus">
         <div className="menu-item" onClick={() => navigate('/home')}>Home</div>
