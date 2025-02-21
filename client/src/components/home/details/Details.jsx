34103c2fcbe511ef4553b72b5fb80ecd9962c64e
anmo2834 <anmolsinha4321@gmail.com>
2025-02-21 23:06:57 +0530

    fixing bugs

--- client/src/components/home/details/Details.jsx
+++ client/src/components/home/details/Details.jsx
@@ -2,10 +2,10 @@ import React, { useContext } from 'react'
 
 import './details.css'
 import { scheduleContext } from '../../context/Schedule'
-import { tokenContext } from '../../context/Token'
+
 function Details() {
   const { mon, tue, wed, thu, fri, leave } = useContext(scheduleContext);
-  const { token, setToken } = useContext(tokenContext);
+  
   let day = new Date().getDay();
   let schedule;
   switch (day) {
@@ -54,21 +54,7 @@ function Details() {
             ))}
           </div>
 
-          <div className='attendance'>
-            {token == 0 ?
-              <div>
-                <h1>subject name</h1>
-                <input type="text" placeholder='Enter Subject Token' />
-                <button>Submit</button>
-              </div>
-              :
-              <div>
-                <h1>subject name</h1>
-                <div>0000</div>
-                <button>Generate Token</button>
-              </div>
-            }
-          </div>
+          <div className='attendance'></div>
         </div>
       </div>
     </div>
